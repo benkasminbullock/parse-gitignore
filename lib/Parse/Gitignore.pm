87a60dbf6b0b9fd6f1fc3bbb55c1f5cdf16b4b29
Ben Bullock <benkasminbullock@gmail.com>
2016-02-03 15:39:06 +0900

    Add initial code and tests

--- lib/Parse/Gitignore.pm
+++ lib/Parse/Gitignore.pm
@@ -8,5 +8,46 @@ require Exporter;
 use warnings;
 use strict;
 use Carp;
+use Path::Tiny;
 our $VERSION = '0.01';
+
+sub get_ignored_files
+{
+    my ($obj, $gitignore_file) = @_;
+    my $file = path ($gitignore_file);
+    $obj->{file} = $file;
+    my @lines = $file->lines ();
+    my %ignored;
+    for my $line (@lines) {
+	if ($line =~ /^\s*#/) {
+	    next;
+	}
+	for my $ignored_file (glob ($line)) {
+	    my $pignored_file = path ($ignored_file);
+	    $ignored{$pignored_file} = 1;
+	}
+    }
+    $obj->{ignored} = \%ignored;
+}
+
+sub new
+{
+    my ($class, $gitignore_file) = @_;
+    if (! -f $gitignore_file) {
+	carp ".gitignore file $gitignore_file doesn't exist";
+	return undef;
+    }
+    my $obj = {};
+    bless $obj, $class;
+    $obj->get_ignored_files ($gitignore_file);
+    return $obj;
+}
+
+sub ignored
+{
+    my ($obj, $file) = @_;
+    my $pfile = path ($file);
+    return $obj->{ignored}{$pfile};
+}
+
 1;
--- t/parse-gitignore.t
+++ t/parse-gitignore.t
@@ -4,6 +4,12 @@ use warnings;
 use strict;
 use Test::More;
 use Parse::Gitignore;
+use FindBin '$Bin';
+chdir "$Bin/test-files" or die "Cannot chdir to test directory: $!";
+my $pg = Parse::Gitignore->new ('test-gitignore');
+ok ($pg->ignored ('monkeyshines'));
+ok ($pg->ignored ('starfruit'));
+ok (! $pg->ignored ('nice'));
 
 done_testing ();
 # Local variables:
